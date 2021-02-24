742ba35a906bc990a5e77a6cdf1fdfca6350d0b0
Yann <ryann72@yahoo.fr>
2021-02-24 08:56:39 +0100

    Changement nom package pour upload pypi

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ README = ""
 
 # This call to setup() does all the work
 setup(
-    name="PrixCarburantClient",
-    version="1.0.6",
-    description="StationEssence client ",
+    name="Prix-Carburant-FR-Client",
+    version="1.0.0",
+    description="StationEssence client",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/ryann72/essence",
     author="Maxime Wantiez, Yann RITTER",
-    author_email="",
+    author_email=" ",
     license="MIT",
     packages=["prixCarburantClient"],
     include_package_data=True,
